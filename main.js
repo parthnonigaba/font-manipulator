d8ac6a700034581007e245ff188e89e51e93cec0
parthnonigaba <85085655+parthnonigaba@users.noreply.github.com>
2022-04-25 19:42:51 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
+
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(500,500);
@@ -6,20 +12,28 @@ function setup(){
     canvas.position(170,550);
 
     poseNet = ml5.poseNet(video, modelloaded);
-    poseNet('pose',gotPoses);
+    poseNet.on('pose',gotPoses);
 
 }
 
 function modelloaded(){
-    console.log("PoseNet has loaded!")
+    console.log("PoseNet has loaded!");
 }
 
 function draw(){
-    background('#00FF00')
+    background('#00FF00');
+    textSize(difference);
+    fill("#0000FF");
+    text('Parth', 20, 30);
 }
 
 function gotPoses(){
     if (results.length > 0){
-        console.log(results)
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX-rightWristX);
+        console.log("left wrist: " + leftWristX+"right wrist: "+rightWristX + "difference: "+ difference);
     }
 }
